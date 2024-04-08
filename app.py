51985b533a89040a6aacd5f4b4f91452831060e6
Marco T. <107734315+marknovanta@users.noreply.github.com>
2024-04-08 17:37:12 +0200

    Initial setup

---
+++ app.py
@@ -0,0 +1,16 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+def configure():
+    load_dotenv()
+
+def main():
+    configure()
+    api_key = os.getenv('api_key')
+
+
+
+
+if __name__ == '__main__':
+    main()
